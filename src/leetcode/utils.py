d448cdb6ee10ca8821fb5035b346db24a27a5065
Cory Nivens <cgn7634@gmail.com>
2022-02-19 00:28:27 -0500

    Adding helper functions for creating linked list test cases

---
+++ src/leetcode/utils.py
@@ -0,0 +1,27 @@
+from typing import List, Optional
+from itertools import zip_longest, tee
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def generate_linked_list(lst: List[int]) -> Optional[ListNode]:
+    if not lst:
+        return None
+
+    last = ListNode(val=lst[-1])
+
+    for val in reversed(lst[:-1]):
+        node = ListNode(val, next=last)
+        last = node
+
+    return last
+
+
+def linked_list_iterator(node):
+    while node is not None:
+        yield node.val
+        node = node.next
